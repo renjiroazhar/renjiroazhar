247aab7f7a8dfc47126e1d6cd1ee8dcb4ef66797
renjiroazhar <renjiroazhar@gmail.com>
2021-02-28 17:50:31 +0700

    Updates

---
+++ precache-manifest.08fd20160c08c0bc2caf46d2f8f30755.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbe14948667ddf72c2357195f3d8c8a",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "ef878a875cd914d83fd9",
+    "url": "/portofolio/static/css/main.265b5fe4.chunk.css"
+  },
+  {
+    "revision": "b4c7eb7899420d02285c",
+    "url": "/portofolio/static/js/2.e5feeb94.chunk.js"
+  },
+  {
+    "revision": "ef878a875cd914d83fd9",
+    "url": "/portofolio/static/js/main.2593c77a.chunk.js"
+  },
+  {
+    "revision": "8ebee44fab047a9cd28e",
+    "url": "/portofolio/static/js/runtime~main.64b239cf.js"
+  },
+  {
+    "revision": "cd2a465b89b58b7f976b346c9f5bd57c",
+    "url": "/portofolio/static/media/portofolio-1.cd2a465b.png"
+  },
+  {
+    "revision": "60aef8d54daf642ca4ef995c7823ed5c",
+    "url": "/portofolio/static/media/portofolio-2.60aef8d5.png"
+  },
+  {
+    "revision": "927230c48a913de7fabd454aa60d804a",
+    "url": "/portofolio/static/media/portofolio-3.927230c4.png"
+  },
+  {
+    "revision": "e40df4d875c2cf95f9eb4b8d7302fad9",
+    "url": "/portofolio/static/media/portofolio-4.e40df4d8.png"
+  },
+  {
+    "revision": "5142ba504dc9bffcef6a7d5d369b6867",
+    "url": "/portofolio/static/media/portofolio-5.5142ba50.png"
+  }
+]);
